62203dcf9e68d1f33e39243f4bc29c15b254aed6
lukas valenzuela <CETECOM@AOAWSB02LC0825.duoc.acad>
2024-04-15 20:35:25 -0400

    subo archivo main

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# #ciclo repeticion for -> para
+# for x in range(3):
+#     print(f"se portaron mal el sabado{x}")
+    
+#     contadorNota = 0
+    
+
+#     cantidad =int (input("ingrese cantidad a de notas a promediar\n"))
+#     for x in range(cantidad):
+#      nota = float(input(f"ingrese nota {x+1} \n "))
+#     contadorNota = contadorNota + nota
+#     promedio = contadorNota/cantidad
+#     promedioredondiado = round(promedio)
+#     print (f"el resultado de las {cantidad} nota es:  {promedioredondiado}")
+    
+bandera = True
+
+while bandera :
+    numero = int(input("ingrese un numero\n"))
+    if numero%2==0 :
+        print ("puede elegir otro numero")
+    else:
+        print("el numero es impar")
+        bandera = False
+        print("muchas gracias por ocupar esta app")
+        
+        a= 10
+        while a >= 10:
+            a = int(input("ingrese numero\n"))
+            print("baicito")
+            
+        
+    
+    
+    
